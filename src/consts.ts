4c3a4a1bf4a5cc1bf3a2dd24c7b88b425564649f
Evan7198 <73023861+Evan7198@users.noreply.github.com>
2025-12-27 16:16:50 +0800

    1.加入引导页 2.修复研究页无法访问的问题 3.其它一些小问题

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '0x6576616E';
+export const SITE_DESCRIPTION = 'Advanced Research in Reverse Engineering, DMA, and Low-Level Security.';
